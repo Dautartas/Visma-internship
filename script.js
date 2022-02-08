3dea9c1996e05d73b4a916b19b9716e8f40d5385
Dautartas <datismcs@gmail.com>
2022-02-08 15:04:35 +0200

    Project folder structure updated, renderProduct() code refactored, Filter values are now calculated, few animations and styling added.

--- script.js
+++ script.js
@@ -1,46 +1,109 @@
-renderShop();
+import { loadData } from "./services/dataService.js";
+let productsLength = await renderShop();
+filter();
 addEventListeners();
 
-async function loadData() {
-  try {
-    const response = await fetch("./data.json");
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    return console.log("Error while fetching the data: " + err.message);
-  }
-}
-
-function renderShop() {
+async function renderShop() {
   let shopGrid = document.querySelector(".shop__grid");
   shopGrid.innerHTML = "";
-  loadData().then((data) => {
-    data["products"].forEach((product) => {
-      let name = product.name;
-      if (name.length > 20) name = name.substring(0, 17) + "...";
-      shopGrid.innerHTML += `<div class="product" id="product-${product.id}">
-              <img src=${product.image} alt="product" class="product__image" />
-              <div class="product__info">
-                <span class="product__info__name" title="${product.name}">${name}</span>
-                <span class="product__info__price">${product.price} €</span>
-              </div>
-              <button class="product__add">Add to cart</button>
-            </div>`;
-      // FIXME:innerHTML
-      if (product.sale === "true") {
-        let element = document.getElementById(`product-${product.id}`);
-        console.log(element);
-        element.innerHTML += `<span class="product__sale">SALE</span>`;
-      }
-    });
+  let data = await loadData();
+  data["products"].forEach((product) => {
+    shopGrid.appendChild(renderProduct(product));
   });
+  console.log(data);
+  console.log(data["products"]);
+  console.log(data["products"].length);
+  return data["products"].length;
 }
+
+/**
+ *
+ * @param {object} data {id,name,price, image}
+ *
+ * @returns {object}
+ * <div>
+ *    <img>
+ *    <div>
+ *        <span></span>
+ *        <span></span>
+ *    </div>
+ *    <button></button>
+ *    <span></span> [Optional]
+ * </div>
+ */
+function renderProduct(data) {
+  let productPrefix = "product";
+
+  let name = data.name;
+  if (name.length > 20) name = name.substring(0, 17) + "...";
+
+  const product__image = document.createElement("img");
+  product__image.className = productPrefix + "__image";
+  product__image.src = data.image;
+  product__image.alt = productPrefix;
+
+  const product__info__name = document.createElement("span");
+  product__info__name.className = productPrefix + "__info__name";
+  product__info__name.title = data.name;
+  product__info__name.appendChild(document.createTextNode(name));
+
+  const product__info__price = document.createElement("span");
+  product__info__price.className = productPrefix + "__info__price";
+  product__info__price.appendChild(document.createTextNode(`${data.price} €`));
+
+  const product__info = document.createElement("div");
+  product__info.className = productPrefix + "__info";
+  product__info.appendChild(product__info__name);
+  product__info.appendChild(product__info__price);
+
+  const product__add = document.createElement("button");
+  product__add.className = productPrefix + "__add";
+  product__add.appendChild(document.createTextNode("Add to cart"));
+
+  if (data.sale === "true") {
+    const product__sale = document.createElement("span");
+    product__sale.className = productPrefix + "__sale";
+    product__sale.appendChild(document.createTextNode("SALE"));
+  }
+
+  const product = document.createElement("div");
+  product.className = productPrefix;
+  product.id = `${productPrefix}-${data.id}`;
+  product.appendChild(product__image);
+  product.appendChild(product__info);
+  product.appendChild(product__add);
+
+  if (data.sale === "true") {
+    const product__sale = document.createElement("span");
+    product__sale.className = productPrefix + "__sale";
+    product__sale.appendChild(document.createTextNode("SALE"));
+    product.appendChild(product__sale);
+  }
+  return product;
+}
+/**
+ * function that calculates data for shop filter box
+ */
+function filter() {
+  setAmount();
+  setInterval();
+}
+
+function setInterval() {
+  document.getElementById("filter__range").innerHTML =
+    "1" + "-" + productsLength;
+}
+
+function setAmount() {
+  document.getElementById("filter__count").innerHTML = productsLength;
+}
+
 function addToCart(event) {
-  console.log(event.target.elements);
+  console.log("Adding to cart..." + event);
 }
 
 function renderCart(event) {
-  console.log("Cart rendering...");
+  console.log("Cart rendering..." + event);
 }
 
 function addEventListeners() {
---
+++ services/dataService.js
@@ -0,0 +1,11 @@
+const dataPath = "../data/data.json";
+
+export async function loadData() {
+  try {
+    const response = await fetch(dataPath);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return console.log("Error while fetching the data: " + err.message);
+  }
+}
