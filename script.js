75c9f5929a7eca591c75d18c40b20bb5e94151e6
Dautartas <datismcs@gmail.com>
2022-02-08 12:24:21 +0200

    Responsive design for small screens fixed, Sale attribute added to data and website

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// classnames as constants?
-// function or const
 renderShop();
 addEventListeners();
 
@@ -12,6 +10,7 @@ async function loadData() {
     return console.log("Error while fetching the data: " + err.message);
   }
 }
+
 function renderShop() {
   let shopGrid = document.querySelector(".shop__grid");
   shopGrid.innerHTML = "";
@@ -19,7 +18,7 @@ function renderShop() {
     data["products"].forEach((product) => {
       let name = product.name;
       if (name.length > 20) name = name.substring(0, 17) + "...";
-      shopGrid.innerHTML += `<div class="product">
+      shopGrid.innerHTML += `<div class="product" id="product-${product.id}">
               <img src=${product.image} alt="product" class="product__image" />
               <div class="product__info">
                 <span class="product__info__name" title="${product.name}">${name}</span>
@@ -27,6 +26,12 @@ function renderShop() {
               </div>
               <button class="product__add">Add to cart</button>
             </div>`;
+      // FIXME:innerHTML
+      if (product.sale === "true") {
+        let element = document.getElementById(`product-${product.id}`);
+        console.log(element);
+        element.innerHTML += `<span class="product__sale">SALE</span>`;
+      }
     });
   });
 }
