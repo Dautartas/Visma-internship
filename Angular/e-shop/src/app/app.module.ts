a5d8450dbdf9241c06998d29a945f9d15a482fe5
Dautartas <datismcs@gmail.com>
2022-02-21 15:19:38 +0200

    Sidebar, footer, header mooved to core folder.

--- Angular/e-shop/src/app/app.module.ts
+++ Angular/e-shop/src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
+import { FooterComponent } from './core/footer/footer.component';
 import { ShopComponent } from './shop/shop.component';
 import { CartComponent } from './cart/cart.component';
 import { FilterComponent } from './shop/filter/filter.component';
 import { ProductListComponent } from './shop/product-list/product-list.component';
 import { ProductComponent } from './shop/product-list/product/product.component';
-import { SidebarSectionComponent } from './sidebar/sidebar-section/sidebar-section.component';
+import { SidebarSectionComponent } from './core/sidebar/sidebar-section/sidebar-section.component';
 @NgModule({
   declarations: [
     AppComponent,
