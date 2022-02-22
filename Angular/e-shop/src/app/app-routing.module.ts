eeedd22983bfccc7ff479092b43c1a953910b58b
Dautartas <datismcs@gmail.com>
2022-02-22 15:00:36 +0200

    Child routes for shop.

--- Angular/e-shop/src/app/app-routing.module.ts
+++ Angular/e-shop/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import { ProductListComponent } from './shop/product-list/product-list.component';
 import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
-  { path: '', component: ShopComponent },
-  { path: 'shop', component: ShopComponent },
+  {
+    path: '',
+    component: ShopComponent,
+    children: [
+      { path: '', component: ProductListComponent },
+      // { path: ':id', component: ShopComponent },
+      // { path: 'add', component: ShopComponent },
+    ],
+  },
   { path: 'cart', component: CartComponent },
 ];
 
